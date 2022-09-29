b02adf418eeaae3aed0ac142a5f187b886b64ecf
Karisa Kalume <72358512+karisa537@users.noreply.github.com>
2022-09-29 11:00:40 +0300

    commented test class

--- src/test/java/com/kcbgroup/main/ken20970customeraccount/Ken20970CustomerAccountApplicationTests.java
+++ src/test/java/com/kcbgroup/main/ken20970customeraccount/Ken20970CustomerAccountApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kcbgroup.main.ken20970customeraccount;
+// package com.kcbgroup.main.ken20970customeraccount;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Ken20970CustomerAccountApplicationTests {
+// @SpringBootTest
+// class Ken20970CustomerAccountApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
